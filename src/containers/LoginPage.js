2d761175779242e7cb670dec14b3dda9964d1d26
hossein-ketabi <hossein.k.171@gmail.com>
2020-03-22 11:42:27 +0430

    first commit by bottom navigation

---
+++ src/containers/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LoginPage(){
+    return(
+        <div>
+            <header className={"resultHeader"}>
+
+                <button>Login page</button>
+            </header>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ src/redux/reducers/RootReducer.js
@@ -0,0 +1,7 @@
+import {combineReducers} from "redux";
+import {LoginReducer} from "../../components/loginPage/LoginReducer";
+
+export default combineReducers(
+    LoginReducer,
+
+);
