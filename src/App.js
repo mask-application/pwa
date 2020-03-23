eea3b4402146128d57b0008d198ae970da7c3fcf
Mohamad Hossein Alahdadian <alahdadian@viratech.ir>
2020-03-23 19:55:01 +0430

    fix syncing of BottomNavigation and ReactRouter

--- src/App.js
+++ src/App.js
@@ -19,45 +19,39 @@ import './App.css';
 import './_App.scss';
 
 export default function App() {
-    // برای یادگیری Router ها در React می‌تونید از پیوند زیر استفاده کنید:
-    // https://reacttraining.com/react-router/web/guides/quick-start
     return (
-        <ThemeProvider theme={theme}>
-            <Router>
-                <div className="app-container">
-                    <div className="app-content">
-                        <div>
-                            {/**
-                             * A <Switch> looks through its children <Route>s and
-                             * renders the first one that matches the current URL.
-                             */}
-                            <Switch>
-                                <Route path="/" exact>
-                                    <Redirect to="/home" />
-                                </Route>
-                                <Route path="/home" exact>
-                                    <HomePage/>
-                                </Route>
-                                <Route path="/map" exact>
-                                    <MapPage/>
-                                </Route>
-                                <Route path="/my-activities" exact>
-                                    <MyActivitiesPage/>
-                                </Route>
-                                <Route path="/family-activities" exact>
-                                    <FamilyActivitiesPage/>
-                                </Route>
-                                <Route path="/informing" exact>
-                                    <InformingPage/>
-                                </Route>
-                            </Switch>
-                        </div>
-                    </div>
-                    <div className="app-navigation">
-                        <Navigation />
-                    </div>
+        <div className="app-container">
+            <div className="app-content">
+                <div>
+                    {/**
+                     * A <Switch> looks through its children <Route>s and
+                     * renders the first one that matches the current URL.
+                     */}
+                    <Switch>
+                        <Route path="/" exact>
+                            <Redirect to="/home" />
+                        </Route>
+                        <Route path="/home" exact>
+                            <HomePage/>
+                        </Route>
+                        <Route path="/map" exact>
+                            <MapPage/>
+                        </Route>
+                        <Route path="/my-activities" exact>
+                            <MyActivitiesPage/>
+                        </Route>
+                        <Route path="/family-activities" exact>
+                            <FamilyActivitiesPage/>
+                        </Route>
+                        <Route path="/informing" exact>
+                            <InformingPage/>
+                        </Route>
+                    </Switch>
                 </div>
-            </Router>
-        </ThemeProvider>
+            </div>
+            <div className="app-navigation">
+                <Navigation />
+            </div>
+        </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ import {Provider} from "react-redux";
 import Store from "./redux/Store";
 import {intl} from "./intl";
 import {RawIntlProvider} from "react-intl";
+import theme from "./theme";
+import {BrowserRouter as Router} from "react-router-dom";
+import {ThemeProvider} from "@material-ui/core";
 
 ReactDOM.render(
     <Provider store={Store}>
         <RawIntlProvider value={intl.reactIntl}>
             <React.StrictMode>
-            <App/>
+                <ThemeProvider theme={theme}>
+                    <Router>
+                        <App/>
+                    </Router>
+                </ThemeProvider>
             </React.StrictMode>
         </RawIntlProvider>
     </Provider>,
