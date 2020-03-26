0d7d03973c8d34fbdfc759073b4f79b50a3a61d1
Amir Reza <amirrezadm81@gmail.com>
2020-03-26 13:14:23 +0430

    add qr code libaray and add icon

--- src/components/AppHeader/Header.js
+++ src/components/AppHeader/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./HeaderStyle.scss";
-import {PersianLan} from "../../constants/Strings";
-import {AppBar, Toolbar, Typography} from "@material-ui/core";
+import { PersianLan } from "../../constants/Strings";
+import { AppBar, Toolbar, Typography, Box } from "@material-ui/core";
 
-export default function Header(props) {
-    return (
-        <AppBar position="static">
-            <Toolbar variant="dense">
-                <Typography variant="h6" color="inherit">
-                    {PersianLan.app_header}
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    )
-
+export default function Header({ leftComponent }) {
+  return (
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <Box className="title">
+          <Typography variant="h6" color="inherit">
+            {PersianLan.app_header}
+          </Typography>
+        </Box>
+        {leftComponent ? leftComponent : null}
+      </Toolbar>
+    </AppBar>
+  );
 }
--- src/containers/MyActivitiesPage.js
+++ src/containers/MyActivitiesPage.js
@@ -1,14 +1,25 @@
 import React from "react";
 import MyActivities from "../components/MyActivities/MyActivities";
 import Header from "../components/AppHeader/Header";
+import { Box, IconButton, SvgIcon } from "@material-ui/core";
+import { CropFree } from "@material-ui/icons";
+import QrCode from "../assets/svg/qr-code.svg";
 
 function MyActivitiesPage() {
-    return (
-        <>
-            <Header/>
-            <MyActivities/>
-        </>
-    )
+  return (
+    <>
+      <Header
+        leftComponent={
+          <Box>
+            <IconButton>
+              <img src={QrCode} width="20" height="20" />
+            </IconButton>
+          </Box>
+        }
+      />
+      <MyActivities />
+    </>
+  );
 }
 
 export default MyActivitiesPage;
