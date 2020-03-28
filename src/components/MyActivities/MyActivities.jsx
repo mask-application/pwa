e70f52a4b1dc417d8f2957cf479ef4e512f40620
hossein-ketabi <hossein.k.171@gmail.com>
2020-03-28 20:56:37 +0430

    resolve conflict & refactor

--- src/components/MyActivities/MyActivities.jsx
+++ src/components/MyActivities/MyActivities.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import NotSignedUp from './pages/NotSignedUp/NotSignedUp';
 import SignUp from './pages/SignUp/SignUp';
 import Activation from './pages/Activation/Activation';
-import MyActiActivitiesLogined from './pages/Loggedin/MyActivitiesLogined';
+import MyActivitiesLoggedin from './pages/Loggedin/MyActivitiesLoggedin';
 
 import {
-    showSignUpPage,
-    showNotSignedUpPage,
-    showActivationPage,
-    activateUser,
-} from "./MyActivitiesActions";
+  showSignUpPage,
+  showNotSignedUpPage,
+  showActivationPage,
+  activateUser,
+} from './MyActivitiesActions';
 
 // TODO remove this useless comments and add a good explanation what does this do, dear @alimo :)
 /**
@@ -20,30 +20,30 @@ import {
  */
 
 export default function MyActivities() {
-    const state = useSelector(store => store.MyActivities);
-    const dispatch = useDispatch();
+  const state = useSelector((store) => store.MyActivities);
+  const dispatch = useDispatch();
 
-    switch (state.page) {
-        case 'INDEX':
-            // FIXME use formattedMessage from react-intl
-            return <MyActiActivitiesLogined/>;
-        case 'NOT_SIGNED_UP':
-            return <NotSignedUp onSignUpClick={() => dispatch(showSignUpPage())} />;
-        case 'SIGN_UP':
-            return (
-                <SignUp
-                    onBackClick={() => dispatch(showNotSignedUpPage())}
-                    onSMSSent={(data) => dispatch(showActivationPage(data))}
-                />
-            );
-        case 'ACTIVATION':
-            return (
-                <Activation
-                    onBackClick={() => dispatch(showSignUpPage())}
-                    onActivate={(data) => dispatch(activateUser(data))}
-                />
-            );
-        default:
-            return null;
-    }
+  switch (state.page) {
+    case 'INDEX':
+      // FIXME use formattedMessage from react-intl
+      return <MyActivitiesLoggedin />;
+    case 'NOT_SIGNED_UP':
+      return <NotSignedUp onSignUpClick={() => dispatch(showSignUpPage())} />;
+    case 'SIGN_UP':
+      return (
+        <SignUp
+          onBackClick={() => dispatch(showNotSignedUpPage())}
+          onSMSSent={(data) => dispatch(showActivationPage(data))}
+        />
+      );
+    case 'ACTIVATION':
+      return (
+        <Activation
+          onBackClick={() => dispatch(showSignUpPage())}
+          onActivate={(data) => dispatch(activateUser(data))}
+        />
+      );
+    default:
+      return null;
+  }
 }
