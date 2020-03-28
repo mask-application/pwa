8cd8145ede4dbd969712f724c45ef43ab8a615bf
ns.sajadi <hwe.sajadi@yahoo.com>
2020-03-29 01:26:16 +0430

    add notification button

--- src/components/AppHeader/Header.js
+++ src/components/AppHeader/Header.js
@@ -1,11 +1,35 @@
-import React from "react";
-import "./HeaderStyle.scss";
-import logo from "../../logo.png";
+import React, { useState } from 'react';
+import './HeaderStyle.scss';
+import logo from '../../logo.png';
+import notification from '../../assets/images/Notification.svg';
+import { Box, Badge, Dialog } from '@material-ui/core';
 
-export default function Header(props) {
+export default function Header() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleNotif = () => {
+    setIsDialogOpen(!isDialogOpen);
+  };
+
   return (
-    <div className="logo">
-      <img src={logo} alt="logo"/>
+    <div className="HeaderWrapper" onClick={() => handleNotif()}>
+      <div className="notif-button-wrapper">
+        <Badge variant="dot" color="primary">
+          <button type="button">
+            <img src={notification} alt="Notification" />
+          </button>
+        </Badge>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+
+      <Dialog open={isDialogOpen}>
+        <Box ml={3} className="notif-msg">
+          این برنامه در حال به روز رسانی می‌باشد، برای بهره‌مندی از جدیدترین
+          امکانات، به‌روز باشید!
+        </Box>
+      </Dialog>
     </div>
-  )
+  );
 }
