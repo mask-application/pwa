ed6e7eec27dced5a722d50e2796044c4164adde8
hossein-ketabi <hossein.k.171@gmail.com>
2020-03-28 22:56:00 +0430

    resolve a bug

--- src/components/MyActivities/MyActivitiesReducer.js
+++ src/components/MyActivities/MyActivitiesReducer.js
@@ -1,4 +1,4 @@
-import * as ActionTypes from "../../redux/actions/ActionTypes";
+import * as ActionTypes from '../../redux/actions/ActionTypes';
 
 // The authentication token. Should be sent via "Access-Token" header.
 const token = localStorage.getItem('token');
@@ -6,60 +6,61 @@ const token = localStorage.getItem('token');
 // User data stored in storage.
 let user;
 try {
-    user = JSON.parse(localStorage.getItem('user'));
-} catch(e) {
-    // User data in storage might be broken
-    user = null;
+  user = JSON.parse(localStorage.getItem('user'));
+} catch (e) {
+  // User data in storage might be broken
+  user = null;
 }
 
 const initialState = {
-    token,
-    user,
-    page: token ? 'INDEX' : 'NOT_SIGNED_UP',
-    phone: null,
-    condition: null,
-    ttl: null,
+  token,
+  user,
+  page: token ? 'INDEX' : 'NOT_SIGNED_UP',
+  phone: null,
+  condition: null,
+  ttl: null,
 
-    eventResult:JSON.parse(localStorage.getItem("eventResult")),
-    eventCounter:localStorage.getItem("eventCounter") || 0,
-    showNavBar:true,
+  eventResult: JSON.parse(localStorage.getItem('eventResult')),
+  eventCounter: localStorage.getItem('eventCounter') || 0,
+  showNavBar: true,
 };
 
 export function MyActivitiesReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'SHOW_SIGN_UP_PAGE':
-            return {
-                ...state,
-                page: 'SIGN_UP',
-            };
-        case 'SHOW_NOT_SIGNED_UP_PAGE':
-            return {
-                ...state,
-                page: 'NOT_SIGNED_UP',
-            };
-        case 'SHOW_ACTIVATION_PAGE':
-            return {
-                ...state,
-                page: 'ACTIVATION',
-                phone: action.phone,
-                condition: action.condition,
-                ttl: action.ttl,
-            };
-        case 'ACTIVATE_USER':
-            localStorage.setItem('token', action.token);
-            localStorage.setItem('user', JSON.stringify(action.user));
-            return {
-                ...state,
-                page: 'INDEX',
-                token: action.token,
-                user: action.user,
-            };
-        case ActionTypes.SAVE_SUCCESS_EVENT_RESPONSE_TO_STATE:
-            return {
-                ...state,
-                eventResult:action.eventResult
-            }
-        default:
-            return state;
-    };
+  switch (action.type) {
+    case 'SHOW_SIGN_UP_PAGE':
+      return {
+        ...state,
+        page: 'SIGN_UP',
+      };
+    case 'SHOW_NOT_SIGNED_UP_PAGE':
+      return {
+        ...state,
+        page: 'NOT_SIGNED_UP',
+      };
+    case 'SHOW_ACTIVATION_PAGE':
+      return {
+        ...state,
+        page: 'ACTIVATION',
+        phone: action.phone,
+        condition: action.condition,
+        ttl: action.ttl,
+      };
+    case 'ACTIVATE_USER':
+      localStorage.setItem('token', action.token);
+      localStorage.setItem('user', JSON.stringify(action.user));
+      return {
+        ...state,
+        page: 'INDEX',
+        token: action.token,
+        user: action.user,
+      };
+    case ActionTypes.SAVE_SUCCESS_EVENT_RESPONSE_TO_STATE:
+      return {
+        ...state,
+        eventResult: action.eventResult,
+        eventCounter: action.eventCounter,
+      };
+    default:
+      return state;
+  }
 }
