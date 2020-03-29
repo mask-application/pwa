9a47e724f6f75f6d32fbcfdcb0208f7697058365
hasan <sahand.e.ma@gmail.com>
2020-03-29 23:24:25 +0430

    Addresses should be relative for URL of services

--- src/components/StatisticalChart/StatisticalChartActions.js
+++ src/components/StatisticalChart/StatisticalChartActions.js
@@ -1,32 +1,32 @@
-export const FETCH_REQUEST   = 'FETCH_REQUEST';
-export const FETCH_SUCCESS   = 'FETCH_SUCCESS';
-export const FETCH_ERROR   = 'FETCH_ERROR';
+export const FETCH_REQUEST = 'FETCH_REQUEST';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 const fetchDataRequest = () => ({
-  type: FETCH_REQUEST
+  type: FETCH_REQUEST,
 });
 
 const fetchDataSuccess = (payload) => ({
   type: FETCH_SUCCESS,
-  payload
+  payload,
 });
 
 const fetchDataError = () => ({
-  type: FETCH_ERROR
+  type: FETCH_ERROR,
 });
 
 export function fetchData() {
-  const URL = "http://185.97.116.63:8001/data/infected.json";
-  return dispatch => {
+  const URL = '/data/infected.json';
+  return (dispatch) => {
     dispatch(fetchDataRequest());
     return fetch(URL)
       .then(handleErrors)
-      .then(res => res.json())
-      .then(json => {
+      .then((res) => res.json())
+      .then((json) => {
         dispatch(fetchDataSuccess(json));
         return json;
       })
-      .catch(error => dispatch(fetchDataError(error)));
+      .catch((error) => dispatch(fetchDataError(error)));
   };
 }
 
