ed45c7781135c58a466cb8413f1b705c971b8c10
ns.sajadi <hwe.sajadi@yahoo.com>
2020-03-29 05:55:04 +0430

    fix bug

--- src/components/AppHeader/Header.js
+++ src/components/AppHeader/Header.js
@@ -1,38 +1,54 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './HeaderStyle.scss';
 import logo from '../../logo.png';
 import notification from '../../assets/images/Notification.svg';
 import { Box, Badge, Dialog, Typography } from '@material-ui/core';
 
-export default function Header() {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDialogOpen: false,
+    };
+    this.handleOpen = this.handleOpen.bind(this);
+  }
 
-  const handleNotif = () => {
-    setIsDialogOpen(!isDialogOpen);
-  };
+  handleOpen(event) {
+    if (event.target.className === 'notif-btn')
+      this.setState({ isDialogOpen: true });
+    else this.setState({ isDialogOpen: false });
+  }
 
-  return (
-    <div className="HeaderWrapper" onClick={() => handleNotif()}>
-      <div className="notif-button-wrapper">
-        <Badge variant="dot" color="primary">
-          <button type="button">
-            <img src={notification} alt="Notification" />
-          </button>
-        </Badge>
-        <div className="logo">
-          <img src={logo} alt="logo" />
+  render() {
+    return (
+      <div className="HeaderWrapper" name="close" onClick={this.handleOpen}>
+        <div className="notif-button-wrapper">
+          <Badge variant="dot" color="primary">
+            <button type="button" name="open" onClick={this.handleOpen}>
+              <img
+                src={notification}
+                className="notif-btn"
+                alt="Notification"
+              />
+            </button>
+          </Badge>
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <Typography variant="subtitle2" className="beta-version">
+            نسخه آزمایشی
+          </Typography>
         </div>
-        <Typography variant="subtitle2" className="beta-version">
-          نسخه آزمایشی
-        </Typography>
-      </div>
 
-      <Dialog open={isDialogOpen}>
-        <Box ml={3} className="notif-msg">
-          این برنامه در حال به روز رسانی می‌باشد، برای بهره‌مندی از جدیدترین
-          امکانات، به‌روز باشید!
-        </Box>
-      </Dialog>
-    </div>
-  );
+        <Dialog open={this.state.isDialogOpen}>
+          <Box ml={3} className="notif-msg">
+            این برنامه در حال به روز رسانی می‌باشد، برای بهره‌مندی از جدیدترین
+            امکانات، به‌روز باشید!
+          </Box>
+        </Dialog>
+      </div>
+    );
+  }
 }
+
+export default Header;
