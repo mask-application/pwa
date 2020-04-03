bf9864e564a5b011061da0d72a81a83d245355f3
ns.sajadi <hwe.sajadi@yahoo.com>
2020-04-03 16:58:31 +0430

    local url

--- src/components/StatisticalChart/StatisticalChartActions.js
+++ src/components/StatisticalChart/StatisticalChartActions.js
@@ -1,33 +1,33 @@
-export const FETCH_REQUEST   = 'FETCH_REQUEST';
-export const FETCH_SUCCESS   = 'FETCH_SUCCESS';
-export const FETCH_ERROR   = 'FETCH_ERROR';
+export const FETCH_REQUEST = 'FETCH_REQUEST';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 const fetchDataRequest = () => ({
-  type: FETCH_REQUEST
+  type: FETCH_REQUEST,
 });
 
 const fetchDataSuccess = (payload) => ({
   type: FETCH_SUCCESS,
-  payload
+  payload,
 });
 
 const fetchDataError = () => ({
-  type: FETCH_ERROR
+  type: FETCH_ERROR,
 });
 
 export function fetchData() {
-  // const URL = "/data/infected.json";
-  const URL = "https://cdn-dev.covidapp.ir/data/infected.json";
-  return dispatch => {
+  // const URL = "http://185.97.116.63:8001/data/infected.json";
+  const URL = `${process.env.REACT_APP_GET_INFECTED_JSON_DATA}`;
+  return (dispatch) => {
     dispatch(fetchDataRequest());
     return fetch(URL)
       .then(handleErrors)
-      .then(res => res.json())
-      .then(json => {
+      .then((res) => res.json())
+      .then((json) => {
         dispatch(fetchDataSuccess(json));
         return json;
       })
-      .catch(error => dispatch(fetchDataError(error)));
+      .catch((error) => dispatch(fetchDataError(error)));
   };
 }
 
