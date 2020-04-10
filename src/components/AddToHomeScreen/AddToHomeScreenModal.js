536e828ed1e3f39cab4bb6abd0584a96f7c76ea0
Alireza Heidari <itisAliRH@gmail.com>
2020-04-10 22:21:45 +0430

    Change to functional component and use fa.json

--- src/components/AddToHomeScreen/AddToHomeScreenModal.js
+++ src/components/AddToHomeScreen/AddToHomeScreenModal.js
@@ -1,68 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './AddToHomeScreenModalStyle.scss';
 import { isIos, isInStandaloneMode } from '../../utils/isIOS';
 import { Button, Dialog, Paper } from '@material-ui/core';
 import logo from '../../logo.png';
 import ShareIcon from '../../assets/images/ShareIcon.svg';
 import AddToHomeScreenIcon from '../../assets/images/AddToHomeScreenIcon.svg';
+import { useIntl } from '../../intl';
 
-class AddToHomeScreenModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalShow: true,
-    };
-    this.handleOnCloseButtonClick = this.handleOnCloseButtonClick.bind(this);
-  }
+export default function AddToHomeScreenModal() {
+  const intl = useIntl();
+  const [modalShow, setModalShow] = useState(true);
 
-  handleOnCloseButtonClick() {
-    this.setState({ modalShow: false });
-  }
+  const handleOnCloseButtonClick = () => {
+    setModalShow(false);
+  };
 
-  render() {
-    const { modalShow } = this.state;
-    const handleOnCloseButtonClick = this.handleOnCloseButtonClick;
-    const shouldRender = modalShow && isIos() && !isInStandaloneMode();
+  const shouldRender = modalShow && isIos() && !isInStandaloneMode();
 
-    return (
-      <div>
-        {shouldRender && (
-          <Dialog fullScreen open={modalShow} className="add-to-home-screen">
-            <div className="logo">
-              <img src={logo} alt="logo" />
-            </div>
-            <span className="flex-column perfect-center">
-              نصب نسخه پیشرو ماسک (مخصوص کاربران آیفون)
+  return (
+    <div>
+      {shouldRender && (
+        <Dialog fullScreen open={modalShow} className="add-to-home-screen">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <span className="flex-column perfect-center">
+            {intl.formatMessage('add-to-home.title')}
+          </span>
+          <Paper className="steps flex-column perfect-center">
+            <span>
+              {intl.formatMessage('add-to-home.step1.first')}
+              <img className="share-icon" src={ShareIcon} alt="Share" />
+              {intl.formatMessage('add-to-home.step1.end')}
             </span>
-            <Paper className="steps flex-column perfect-center">
-              <span>
-                ۱- در نوار پایین، روی
-                <img className="share-icon" src={ShareIcon} alt="Share" />
-                کلیک کنید.
-              </span>
-              <span>
-                ۲- در منوی باز شده روی
-                <img
-                  className="add-to-home-screen-icon"
-                  src={AddToHomeScreenIcon}
-                  alt="Add To Home Screen"
-                />
-                کلیک کنید.
-              </span>
-              <span>۳- در آخر در بالای صفحه دکمه Add را کلیک کنید.</span>
-            </Paper>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleOnCloseButtonClick}
-            >
-              بعدا
-            </Button>
-          </Dialog>
-        )}
-      </div>
-    );
-  }
+            <span>
+              {intl.formatMessage('add-to-home.step2.first')}
+              <img
+                className="add-to-home-screen-icon"
+                src={AddToHomeScreenIcon}
+                alt="Add To Home Screen"
+              />
+              {intl.formatMessage('add-to-home.step2.end')}
+            </span>
+            <span>{intl.formatMessage('add-to-home.step3.first')}</span>
+          </Paper>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOnCloseButtonClick}
+          >
+            بعدا
+          </Button>
+        </Dialog>
+      )}
+    </div>
+  );
 }
-
-export default AddToHomeScreenModal;
