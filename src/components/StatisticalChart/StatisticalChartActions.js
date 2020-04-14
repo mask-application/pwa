77bd5acbb613b9555948a0267fa5261f3fa62494
ns.sajadi <hwe.sajadi@yahoo.com>
2020-04-14 16:34:48 +0430

    delete local URL

--- src/components/StatisticalChart/StatisticalChartActions.js
+++ src/components/StatisticalChart/StatisticalChartActions.js
@@ -16,8 +16,7 @@ const fetchDataError = () => ({
 });
 
 export function fetchData() {
-  const URL = 'http://185.97.116.63:3000/data/infected.json';
-  // const URL = `${process.env.REACT_APP_GET_INFECTED_JSON_DATA}`;
+  const URL = `${process.env.REACT_APP_GET_INFECTED_JSON_DATA}`;
   return (dispatch) => {
     dispatch(fetchDataRequest());
     return fetch(URL)
