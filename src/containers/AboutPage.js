fb7948f0419c963d161347cd71b3231055ea2fe9
faridghafooriadl <faridghafooriadl@gmail.com>
2020-04-14 15:18:03 +0430

    add about page and finished request to http://185.97.116.63:3000/data/about.html url and get data and show in this page

---
+++ src/containers/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { About } from "../components/About/About";
+
+function AboutPage() {
+  return (
+    <div>
+      <About/>
+    </div>
+  );
+}
+
+export default AboutPage;
