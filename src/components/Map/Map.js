1b02fa916faeef0cf72b85ecd03856273a7e3c6f
Golnaz Adib <golnaz@Golnazs-MacBook-Pro.local>
2020-04-20 03:38:04 +0430

    bugfix: map find zoom level

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -110,30 +110,39 @@ function Map() {
     }
   };
 
-  useEffect(() => {
-    const findZoom = () => {
-      const inverseZoomLevel = 10 * Math.pow(2, -(map && map.getZoom()));
-      const zoomLevels = data && data.map((element) => element[0]);
-      for (let i = 0; i < zoomLevels.length - 1; i++) {
-        if (inverseZoomLevel < zoomLevels[i]) {
-          setZoom(i);
-          break;
-        } else if (
+  const findZoomLevels = useCallback(() => {
+    const result = [];
+    data.map((element) => result.push(element[0]));
+    return result;
+  }, [data]);
+
+  const findZoom = useCallback(() => {
+    const inverseZoomLevel = 10 * Math.pow(2, -(map && map.getZoom()));
+    const zoomLevels = data && findZoomLevels();
+    for (let i = 0; i < zoomLevels.length - 1; i++) {
+      if (inverseZoomLevel < zoomLevels[i]) {
+        setZoom(i);
+        break;
+      } else if (
           inverseZoomLevel >= zoomLevels[i] &&
           inverseZoomLevel < zoomLevels[i + 1]
-        ) {
-          setZoom(i + 1);
-          break;
-        }
+      ) {
+        setZoom(i + 1);
+        break;
       }
-    };
+    }
+  }, [map, data, findZoomLevels]);
 
-    if (!map || !data) return;
-
+  useEffect(() => {
     findZoom();
-    map.on('zoom', findZoom);
-    return () => map.off(findZoom);
-  }, [data, map]);
+  }, [findZoom, data]);
+
+  useEffect(() => {
+    map &&
+    map.on('zoom', function () {
+      findZoom();
+    });
+  });
 
   useEffect(() => {
     dispatch(fetchMap());
