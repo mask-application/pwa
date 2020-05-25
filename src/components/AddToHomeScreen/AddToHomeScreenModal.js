537f668c7478b90e7140c6b4cc63a31099495bac
danialfarsi <danialfarsi@yahoo.com>
2020-05-26 00:40:14 +0430

    changing

--- src/components/AddToHomeScreen/AddToHomeScreenModal.js
+++ src/components/AddToHomeScreen/AddToHomeScreenModal.js
@@ -8,9 +8,11 @@ import AndroidChromeMenu from '../../assets/images/AndroidChromeMenu.svg';
 import AddToHomeScreenIcon from '../../assets/images/AddToHomeScreenIcon.svg';
 import { useIntl } from '../../intl';
 import { isAndroid, isMobile, isIOS, isIPad13 } from 'react-device-detect';
+import { useHistory } from 'react-router-dom';
 
 export default function AddToHomeScreenModal() {
   const intl = useIntl();
+  let history = useHistory();
   const [modalShow, setModalShow] = useState(true);
 
   const handleOnCloseButtonClick = () => {
@@ -34,55 +36,72 @@ export default function AddToHomeScreenModal() {
           <div className="logo">
             <img src={logo} alt="logo" />
           </div>
-          <h3 className="flex-column perfect-center">
-            {isAndroid && intl.formatMessage('add-to-home.android.title')}
-            {(isIOS || isIPad13) && intl.formatMessage('add-to-home.ios.title')}
-          </h3>
-          <Paper className="steps flex-column perfect-center">
-            <span>
-              {(isIOS || isIPad13) &&
-                intl.formatMessage('add-to-home.ios.step1.first')}
-              {(isIOS || isIPad13) && (
-                <img className="share-icon" src={ShareIcon} alt="Share" />
-              )}
-              {isAndroid &&
-                intl.formatMessage('add-to-home.android.step1.first')}
-              {isAndroid && (
-                <img
-                  className="chrome-menu"
-                  src={AndroidChromeMenu}
-                  alt="Menu"
-                />
-              )}
-              {intl.formatMessage('add-to-home.ios.step1.end')}
-            </span>
-            <span>
-              {intl.formatMessage('add-to-home.step2.first')}
-              {(isIOS || isIPad13) && (
-                <img
-                  className="add-to-home-screen-icon"
-                  src={AddToHomeScreenIcon}
-                  alt="Add To Home Screen"
-                />
-              )}
-              {isAndroid && (
-                <span className="add-to-home-screen-icon">
-                  {intl.formatMessage(
-                    'add-to-home.android.step2.add-to-home-screen'
+
+          {(isIOS || isIPad13) && (
+            <>
+              <h3 className="flex-column perfect-center text">
+                {(isIOS || isIPad13) &&
+                  intl.formatMessage('add-to-home.ios.title')}
+              </h3>
+              <Paper className="steps flex-column perfect-center">
+                <span>
+                  {(isIOS || isIPad13) &&
+                    intl.formatMessage('add-to-home.ios.step1.first')}
+                  {(isIOS || isIPad13) && (
+                    <img className="share-icon" src={ShareIcon} alt="Share" />
                   )}
+
+                  {intl.formatMessage('add-to-home.ios.step1.end')}
                 </span>
-              )}
-              {intl.formatMessage('add-to-home.step2.end')}
-            </span>
-            <span>{intl.formatMessage('add-to-home.step3.first')}</span>
-          </Paper>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleOnCloseButtonClick}
-          >
-            {intl.formatMessage('add-to-home.add-later')}
-          </Button>
+                <span>
+                  {intl.formatMessage('add-to-home.step2.first')}
+                  {(isIOS || isIPad13) && (
+                    <img
+                      className="add-to-home-screen-icon"
+                      src={AddToHomeScreenIcon}
+                      alt="Add To Home Screen"
+                    />
+                  )}
+                  {intl.formatMessage('add-to-home.step2.end')}
+                </span>
+                <span>{intl.formatMessage('add-to-home.step3.first')}</span>
+              </Paper>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleOnCloseButtonClick}
+              >
+                {intl.formatMessage('add-to-home.add-later')}
+              </Button>
+            </>
+          )}
+          {isAndroid && (
+            <>
+              <Paper className="steps flex-column perfect-center">
+                <h3 className="flex-column perfect-center text">
+                  {isAndroid &&
+                    intl.formatMessage('add-to-home.android.title1')}
+                </h3>
+              </Paper>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  history.push('/application.html');
+                }}
+              >
+                {intl.formatMessage('add-to-home.down-android')}
+              </Button>
+              <Button
+                className="btn_web"
+                variant="contained"
+                color="primary"
+                onClick={handleOnCloseButtonClick}
+              >
+                {intl.formatMessage('add-to-home.web')}
+              </Button>
+            </>
+          )}
         </Dialog>
       )}
     </div>
